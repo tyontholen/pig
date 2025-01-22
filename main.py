c4c99ac7c0ab6bcd74ab0210dd5d16ce5effd0e1
Gustav <tyontholen@gmail.com>
2025-01-22 19:49:43 +0100

    feat: main game

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import random
 
 #dice roll function
 def roll():
-    min_value = 1
+    min_value = 2
     max_value = 6
     roll = random.randint(min_value, max_value)
 
@@ -26,11 +26,34 @@ while True:
 max_score = 50
 player_scores = [0 for _ in range(players)]
 
-#print(player_scores)
+
+#loop trough the player turns
 
 while max(player_scores) < max_score:
-    should_roll = input("Would you like to roll? (y) ")
-    if should_roll.lower() != "y":
-        break
+    for player_i in range(players):
+        print(f"Player number {player_i + 1} has just started! \n")
+        print(f"Your total score is: {player_scores[player_i]} \n")
+        current_score = 0
 
-    value = roll()
+        while True:
+            should_roll = input("Would you like to roll? (y) ")
+            if should_roll.lower() != "y":
+                break
+
+            value = roll()
+            if value == 1:
+                print("You rolled a 1! Turn done!")
+                current_score = 0
+                break
+            else:
+                current_score += value
+                print(f"You rolled a: {value}")
+                
+        #sum up the players score after turn        
+        player_scores[player_i] += current_score
+        print(f"Your score is: {current_score}")
+        
+#present the winner            
+max_score = max(player_scores)
+winning_i = player_scores.index(max_score)
+print(f"Player number {winning_i + 1} is the winner with the score of: {max_score}")  
